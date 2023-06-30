51a6092115a73d0a6aaf7cd98efbb89ff7e99e7b
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-01 00:35:44 +0500

    Commited File : login.component.ts at 01/07/2023 12:35:44 am

---
+++ blog-app/client/src/app/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // Perform login logic here
+    if (this.loginForm.valid) {
+      console.log('Email:', this.loginForm.value.email);
+      console.log('Password:', this.loginForm.value.password);
+    }
+  }
+}
