c19ca7f22cfd8d3cb56ba93bf411bbd757f533d5
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-01 12:30:14 +0500

    1-7-2023

--- blog-app/client/src/app/app.module.ts
+++ blog-app/client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +10,6 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
-import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -27,9 +26,11 @@ import {MatCardModule} from "@angular/material/card";
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule,
   ],
-  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
