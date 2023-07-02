517797ccbfd3adff88cfbebc7a29e18200228f99
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-02 14:59:51 +0500

    Commited File : register.component.ts at 02/07/2023 2:59:51 pm

--- blog-app/client/src/app/components/register/register.component.ts
+++ blog-app/client/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserApiHandlerService } from 'src/app/services/user-api-handler.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder,public userApiHandler:UserApiHandlerService,private route:Router) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // Perform login logic here
+    if (this.registerForm.valid) {
+     // @ts-ignore
+     let user:User = {
+       email : this.registerForm.value.email,
+       password : this.registerForm.value.password
+     }
+     this.userApiHandler.login(user).then((data)=>{
+        if(data){
+          alert('Login Successfull');
+          this.route.navigate(['/']);
+        }else{
+          alert('Login Failed');
+        }
+     });
+    }
+  }
 }
