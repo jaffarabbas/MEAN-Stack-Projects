b5d48519ee6c6332343af7b340ac4563851411ad
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-03 17:00:29 +0500

    Commited File : api.service.ts at 03/07/2023 5:00:29 pm

--- blog-app/client/src/app/services/api.service.ts
+++ blog-app/client/src/app/services/api.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { Observable, Subject, tap } from 'rxjs';
+import { LocalStorageService } from './local-storage.service';
+import { json } from 'express';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +15,23 @@ export class ApiService <T> implements OnInit{
 
   private _refreshApi = new Subject<void>();
 
+  protected constructor(protected http: HttpClient) {
+
+  }
+
   get refreshApi() {
     return this._refreshApi;
   }
+
   get apiUrlWithController(): string {
     return this.apiUrl + this.apiController + "/";
   }
 
-  protected constructor(protected http: HttpClient) {}
+  private getHeaders(token:any): HttpHeaders {
+    // Replace with your actual bearer token
+    console.log(token);
+    return new HttpHeaders().set('Authorization', `Bearer ${JSON.parse(token)}`);
+  }
 
   getAll(endPoint:string): Observable<T[]> {
     return this.http.get<T[]>(this.apiUrlWithController+endPoint);
@@ -65,9 +76,72 @@ export class ApiService <T> implements OnInit{
   remove(value: T,id:any,endPoint:string): Observable<T> {
     return this.http.put<T>(`${this.apiUrlWithController+endPoint}/${id}`, value);
   }
-  
+
   delete(id: number,endPoint:string): Observable<any> {
     return this.http.delete(`${this.apiUrlWithController+endPoint}/${id}`).pipe(
+      tap(() => {
+        this.refreshApi.next();
+      })
+    );
+  }
+
+  _getAll(endPoint: string,token:any): Observable<T[]> {
+    const headerData = this.getHeaders(token);
+    return this.http.get<T[]>(this.apiUrlWithController + endPoint, { headers: headerData });
+  }
+
+  _getOne(endPoint: string,token:any): Observable<T> {
+    console.log("token",token);
+    const headerData = this.getHeaders(token);
+    console.log("headerData",headerData);
+    return this.http.get<T>(this.apiUrlWithController + endPoint, { headers: headerData });
+  }
+
+  _getAllById(id: any, endPoint: string,token:any): Observable<T[]> {
+    const headerData = this.getHeaders(token);
+    return this.http.get<T[]>(`${this.apiUrlWithController + endPoint}/${id}`, { headers: headerData });
+  }
+
+  _getById(id: any, endPoint: string,token:any): Observable<T> {
+    const headerData = this.getHeaders(token);
+    return this.http.get<T>(`${this.apiUrlWithController + endPoint}/${id}`, { headers: headerData });
+  }
+
+  _create(item: T, endPoint: string,token:any): Observable<T> {
+    const headerData = this.getHeaders(token);
+    return this.http.post<T>(this.apiUrlWithController + endPoint, item, { headers: headerData }).pipe(
+      tap(() => {
+        this.refreshApi.next();
+      })
+    );
+  }
+
+  _update(value: T, id: any, endPoint: string,token:any): Observable<T> {
+    const headerData = this.getHeaders(token);
+    return this.http.put<T>(`${this.apiUrlWithController + endPoint}/${id}`, value, { headers: headerData }).pipe(
+      tap(() => {
+        this.refreshApi.next();
+      })
+    );
+  }
+
+  _updateByData(value: T, endPoint: string,token:any): Observable<T> {
+    const headerData = this.getHeaders(token);
+    return this.http.put<T>(this.apiUrlWithController + endPoint, value, { headers: headerData }).pipe(
+      tap(() => {
+        this.refreshApi.next();
+      })
+    );
+  }
+
+  _remove(value: T, id: any, endPoint: string,token:any): Observable<T> {
+    const headerData = this.getHeaders(token);
+    return this.http.put<T>(`${this.apiUrlWithController + endPoint}/${id}`, value, { headers: headerData });
+  }
+
+  _delete(id: number, endPoint: string,token:any): Observable<any> {
+    const headerData = this.getHeaders(token);
+    return this.http.delete(`${this.apiUrlWithController + endPoint}/${id}`, { headers: headerData }).pipe(
       tap(() => {
         this.refreshApi.next();
       })
