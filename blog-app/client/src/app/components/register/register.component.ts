d59070de00ed04472e7cf8b8a8a89b7b23c3f294
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-03 17:00:28 +0500

    Commited File : register.component.ts at 03/07/2023 5:00:28 pm

--- blog-app/client/src/app/components/register/register.component.ts
+++ blog-app/client/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from 'src/app/model/user';
 import { UserApiHandlerService } from 'src/app/services/user-api-handler.service';
 
 @Component({
@@ -15,8 +16,11 @@ export class RegisterComponent {
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
+      password_confirmation: ['', Validators.required],
+      tc: ['true', Validators.required]
     });
   }
 
@@ -24,18 +28,32 @@ export class RegisterComponent {
     // Perform login logic here
     if (this.registerForm.valid) {
      // @ts-ignore
-     let user:User = {
-       email : this.registerForm.value.email,
-       password : this.registerForm.value.password
-     }
-     this.userApiHandler.login(user).then((data)=>{
-        if(data){
-          alert('Login Successfull');
+     let user : User = {
+        name:this.registerForm.value.name,
+        email:this.registerForm.value.email,
+        password:this.registerForm.value.password,
+        tc:this.registerForm.value.tc
+     };
+     if(user.password == this.registerForm.value.password_confirmation){
+      let data_with_confirm_password = {
+        ...user,
+        password_confirmation:this.registerForm.value.password
+       };
+       this.userApiHandler.regsiter(data_with_confirm_password).then((res)=>{
+        if(res){
+          alert('Register Successfull');
           this.route.navigate(['/']);
         }else{
-          alert('Login Failed');
+          alert('Register Failed');
         }
-     });
+        }).catch((err)=>{
+          alert('Register Failed');
+        });
+     }else{
+        alert('Password and Confirm Password not matched');
+        return;
+     }
+    
     }
   }
 }
