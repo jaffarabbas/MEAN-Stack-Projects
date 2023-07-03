0eb0b77dffc1f7df34969c30cd5f89d3cf04ba3d
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-03 17:00:30 +0500

    Commited File : user-api-handler.service.ts at 03/07/2023 5:00:29 pm

--- blog-app/client/src/app/services/user-api-handler.service.ts
+++ blog-app/client/src/app/services/user-api-handler.service.ts
@@ -7,16 +7,36 @@ import { ApiUrls } from '../utils/apiUrls';
   providedIn: 'root'
 })
 export class UserApiHandlerService {
-
+  loggedUser!:User;
   constructor(private userService:UserService) { }
 
   async login(user:User) {
-    let {email , password} = user;
     const userData = await this.userService.create(user,ApiUrls.API_ENDPOINTS.user.login).toPromise();
     if(userData){
-      localStorage.setItem('userToken',JSON.stringify(userData));
+      // @ts-ignore
+      localStorage.setItem('userToken',JSON.stringify(userData['token']));
       return true;
     }
     return false;
+  }
+
+  async regsiter(user:User) {
+    const userData = await this.userService.create(user,ApiUrls.API_ENDPOINTS.user.register).toPromise();
+    if(userData){
+      // @ts-ignore
+      localStorage.setItem('userToken',JSON.stringify(userData['token']));
+      return true;
+    }
+    return false;
+  }
+
+  async getLogedUser(){
+    console.log("getLogedUser");
+    const token = localStorage.getItem('userToken');
+    console.log(token);
+    const userData = await this.userService._getOne(ApiUrls.API_ENDPOINTS.user.loggedUser,token).toPromise();
+    if(userData){
+      this.loggedUser = userData;
+    }
   }
 }
