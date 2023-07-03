af21c0c69653dd50a17c1e2bdc4f25e48b3ee4ce
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-03 17:00:27 +0500

    Commited File : dashboard.component.ts at 03/07/2023 5:00:27 pm

--- blog-app/client/src/app/components/dashboard/dashboard.component.ts
+++ blog-app/client/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserApiHandlerService } from 'src/app/services/user-api-handler.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  user:any;
+  constructor(public userServiceApiHandler:UserApiHandlerService) { }
 
+  ngOnInit(): void {
+    this.getLoggedInUser();
+  }
+
+  getLoggedInUser(){
+    console.log("getLoggedInUser");
+    this.userServiceApiHandler.getLogedUser().then((data)=>{
+      // @ts-ignore
+      this.user = this.userServiceApiHandler.loggedUser['user'];
+    });
+  }
 }
