54ced760b944c39e1462ad96936067459202aae3
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-04 10:09:40 +0500

    Commited File : user-api-handler.service.ts at 04/07/2023 10:09:40 am

--- blog-app/client/src/app/services/user-api-handler.service.ts
+++ blog-app/client/src/app/services/user-api-handler.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@angular/core';
 import { UserService } from './user.service';
 import { User } from '../model/user';
 import { ApiUrls } from '../utils/apiUrls';
+import { LocalStorageService } from './local-storage.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiHandlerService {
   loggedUser!:User;
-  constructor(private userService:UserService) { }
+  constructor(private userService:UserService,private localStrorageService:LocalStorageService,private router:Router) { }
 
   async login(user:User) {
     const userData = await this.userService.create(user,ApiUrls.API_ENDPOINTS.user.login).toPromise();
     if(userData){
       // @ts-ignore
-      localStorage.setItem('userToken',JSON.stringify(userData['token']));
+      let token = userData['token'];
+      console.log(token);
+      this.localStrorageService.setItem('userToken',token);
       return true;
     }
     return false;
@@ -23,8 +27,13 @@ export class UserApiHandlerService {
   async regsiter(user:User) {
     const userData = await this.userService.create(user,ApiUrls.API_ENDPOINTS.user.register).toPromise();
     if(userData){
+      console.log(userData);
       // @ts-ignore
-      localStorage.setItem('userToken',JSON.stringify(userData['token']));
+      this.localStrorageService.setItem('userToken',userData['token']);
+
+      console.log("getLogedUser");
+      const token = this.localStrorageService.getItem('userToken');
+      console.log(token);
       return true;
     }
     return false;
@@ -32,11 +41,16 @@ export class UserApiHandlerService {
 
   async getLogedUser(){
     console.log("getLogedUser");
-    const token = localStorage.getItem('userToken');
+    const token = this.localStrorageService.getItem('userToken');
     console.log(token);
     const userData = await this.userService._getOne(ApiUrls.API_ENDPOINTS.user.loggedUser,token).toPromise();
     if(userData){
       this.loggedUser = userData;
     }
+  }
+
+  async logout(){
+    this.localStrorageService.removeItem('userToken');
+    this.router.navigate(['/login']);
   }
 }
