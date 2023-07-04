f595e4ba290dce73bf7e12a800c0b762721a01dd
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-04 10:09:39 +0500

    Commited File : is-user-logged-id.guard.ts at 04/07/2023 10:09:39 am

--- blog-app/client/src/app/guards/is-user-logged-id.guard.ts
+++ blog-app/client/src/app/guards/is-user-logged-id.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { LocalStorageService } from '../services/local-storage.service';
+
 
 export const isUserLoggedIdGuard: CanActivateFn = (route, state) => {
-  return true;
+  let localStorage = new LocalStorageService();
+  let router = new Router();
+  let token = localStorage.getItem('userToken');
+  console.log(token);
+  if (token != null) {
+    return true;
+  }else{
+    router.navigate(['/login']);
+    return false;
+  }
 };
