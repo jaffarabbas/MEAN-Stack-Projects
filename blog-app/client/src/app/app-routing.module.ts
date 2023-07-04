683ec42daff39c83d22f38dbe5e62fcc8952efbd
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-04 10:09:38 +0500

    Commited File : app-routing.module.ts at 04/07/2023 10:09:38 am

--- blog-app/client/src/app/app-routing.module.ts
+++ blog-app/client/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
+import { isUserLoggedIdGuard } from './guards/is-user-logged-id.guard';
 
 
 const routes: Routes = [
-  {path:'',component:DashboardComponent,canActivate:[]},
+  {path:'',component:DashboardComponent,canActivate:[isUserLoggedIdGuard]},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
 ];
